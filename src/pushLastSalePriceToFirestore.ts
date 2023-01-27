b2d7cf4c42c4f7016991709699231ce29c6023a5
nneverlander <findadit@gmail.com>
2023-01-27 10:41:19 -0300

    minor

--- src/pushLastSalePriceToFirestore.ts
+++ src/pushLastSalePriceToFirestore.ts
@@ -43,7 +43,7 @@ const CHAIN_ID = ChainId.Mainnet;
 export const pushLastSalePriceToFs = () => {
   const query =
     'SELECT collection_address, token_id, sale_price_eth, sale_timestamp, block_number FROM eth_nft_sales \
-       WHERE block_number >= 1000000 AND block_number <= 16485264 ORDER BY block_number ASC LIMIT 10000000';
+       WHERE block_number >= 15000000 AND block_number <= 16500000 ORDER BY block_number ASC LIMIT 10000000';
 
   pool.connect((err, client, done) => {
     if (err) {
@@ -68,15 +68,22 @@ export const pushLastSalePriceToFs = () => {
     });
 
     stream.on('data', async (row) => {
-      // console.log(row);
       const collectionAddress = row.collection_address;
       const tokenId = row.token_id;
       const salePriceEth = parseFloat(row.sale_price_eth);
       const saleTimestamp = Number(row.sale_timestamp);
+      
+      let collectionDocId;
+      try {
+        collectionDocId = getCollectionDocId({ collectionAddress, chainId: CHAIN_ID });
+      } catch (err) {
+        console.error(err);
+        return;
+      }
 
       const docRef = firestore
         .collection(firestoreConstants.COLLECTIONS_COLL)
-        .doc(getCollectionDocId({ collectionAddress, chainId: CHAIN_ID }))
+        .doc(collectionDocId)
         .collection(firestoreConstants.COLLECTION_NFTS_COLL)
         .doc(tokenId);
 
