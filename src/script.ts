465b8e610a37663e04954f8962649ccbd9f4cda2
nneverlander <findadit@gmail.com>
2023-04-16 13:13:56 -0500

    minor

--- src/script.ts
+++ src/script.ts
@@ -16,15 +16,15 @@ export async function main(): Promise<void> {
     // await deleteNonSupportedCollLiquidity();
     // pushLastSalePriceToFs();
     // await fetchAllEthNFTSalesFromAlchemy(true);
-    // await updateSaleAndOrderImagesInPostgres();
+    await updateSaleAndOrderImagesInPostgres();
 
-    const filePath = path.join(__dirname, 'supportedColls-ETH-Mainnet.txt');
-    const supportedCollsFile = readFileSync(filePath, 'utf-8');
-    const supportedColls = supportedCollsFile.split('\n');
-    for (const coll of supportedColls) {
-      const chainId = '1';
-      await updateSaleAndOrderImagesForCollId(chainId, coll);
-    }
+    // const filePath = path.join(__dirname, 'supportedColls-ETH-Mainnet.txt');
+    // const supportedCollsFile = readFileSync(filePath, 'utf-8');
+    // const supportedColls = supportedCollsFile.split('\n');
+    // for (const coll of supportedColls) {
+    //   const chainId = '1';
+    //   await updateSaleAndOrderImagesForCollId(chainId, coll);
+    // }
 
     // await fetchNFTSalesFromReservoir('0xe29f8038d1a3445ab22ad1373c65ec0a6e1161a4');
   } catch (err) {
